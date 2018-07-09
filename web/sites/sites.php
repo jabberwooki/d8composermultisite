4c9a55ab00b324abf48eacd1df7a38917f12a388
Christophe Espiau <christophe.espiau@gmail.com>
2018-07-09 17:30:49 +0200

    Création du fichier sites.php.

---
+++ web/sites/sites.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ubuntu
+ * Date: 09/07/18
+ * Time: 17:20
+ */
+
+$sites = array(
+  // Local instances
+  'www.d8composermultisite1.localhost' => 'site1',
+
+  // Korora dev instances
+  'd8composermultisite1.korora.fr' => 'site1',
+);
